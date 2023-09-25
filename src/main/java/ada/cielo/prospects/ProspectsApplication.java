81a7d5c21cd2bc317887403d250f14a805704ef3
marcelobns <marcelobns@live.com>
2023-09-25 19:58:56 -0400

    start spring

---
+++ src/main/java/ada/cielo/prospects/ProspectsApplication.java
@@ -0,0 +1,13 @@
+package ada.cielo.prospects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ada/cielo/prospects/ProspectsApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.cielo.prospects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
